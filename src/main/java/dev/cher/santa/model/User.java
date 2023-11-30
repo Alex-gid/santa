a423721854d246941443d81ec71129fd8c13ab1a
Alex <aleksandrcerkasov8312@gmail.com>
2023-11-30 15:41:57 +0300

    /до рефакторинга

--- src/main/java/dev/cher/santa/model/User.java
+++ src/main/java/dev/cher/santa/model/User.java
@@ -1,11 +1,17 @@
 package dev.cher.santa.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
 import java.sql.Timestamp;
 
+
 @Entity(name ="usersDataTable")
+@Getter
+@Setter
 public class User {
 
     @Id
@@ -16,43 +22,6 @@ public class User {
     private String userName;
     private Timestamp registeredAt;
 
-    public void setChatId(Long chatId) {
-        this.chatId = chatId;
-    }
-
-    public Long getChatId() {return chatId;}
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public Timestamp getRegisteredAt() {
-        return registeredAt;
-    }
-
-    public void setRegisteredAt(Timestamp registeredAt) {
-        this.registeredAt = registeredAt;
-    }
 
     @Override
     public String toString() {
