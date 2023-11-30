4c42e456a7c137557516dc3a7fa6e8869bf379ce
Alex <aleksandrcerkasov8312@gmail.com>
2023-11-30 15:38:27 +0300

    /до рефакторинга

---
+++ src/main/java/dev/cher/santa/model/UserRepository.java
@@ -0,0 +1,6 @@
+package dev.cher.santa.model;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+}
---
+++ src/main/java/dev/cher/santa/service/TelegramBot.java
@@ -0,0 +1,260 @@
+package dev.cher.santa.service;
+
+import com.vdurmont.emoji.EmojiParser;
+import dev.cher.santa.config.BotConfig;
+import dev.cher.santa.model.User;
+import dev.cher.santa.model.UserRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.commands.SetMyCommands;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
+import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageText;
+import org.telegram.telegrambots.meta.api.objects.InputFile;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
+import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScopeDefault;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+@Component
+public class TelegramBot extends TelegramLongPollingBot {
+    private final UserRepository userRepository;
+    final BotConfig config;
+    static final String image = "https://i.ibb.co/FKFBH6B/razdel-v-razrabotke-1.jpg";
+    static final String HELP_TEXT = "This bot is created to demonstrate Spring capabilities. \n\n" +
+            "You can execute commands from the main menu on the left or by typing a command: \n\n" +
+            "Type /start Все подарки в разработке, пожалуйста обратитесь попозже Хо-Хо-Хо \n\n" +
+            "Type /mydata Все подарки в разработке, пожалуйста обратитесь попозже Хо-Хо-Хо \n\n" +
+            "Type /deletedata Все подарки в разработке, пожалуйста обратитесь попозже Хо-Хо-Хо \n\n" +
+            "Type /help to Все подарки в разработке, пожалуйста обратитесь попозже Хо-Хо-Хо \n\n" +
+            "Type /settings to Все подарки в разработке, пожалуйста обратитесь попозже Хо-Хо-Хо \n\n";
+
+
+    @Autowired
+    public TelegramBot(BotConfig config, UserRepository userRepository) {
+        this.config = config;
+        List<BotCommand> listOfCommands = new ArrayList<>();
+        listOfCommands.add(new BotCommand("/start", "Все подарки в разработке, пожалуйста обратитесь попозже Хо-Хо-Хо"));
+        listOfCommands.add(new BotCommand("/mydata", "Все подарки в разработке, пожалуйста обратитесь попозже Хо-Хо-Хо"));
+        listOfCommands.add(new BotCommand("/deletedata", "Все подарки в разработке, пожалуйста обратитесь попозже Хо-Хо-Хо"));
+        listOfCommands.add(new BotCommand("/help", "Все подарки в разработке, пожалуйста обратитесь попозже Хо-Хо-Хо"));
+        listOfCommands.add(new BotCommand("/settings", "Все подарки в разработке, пожалуйста обратитесь попозже Хо-Хо-Хо"));
+        try {
+            this.execute(new SetMyCommands(listOfCommands, new BotCommandScopeDefault(), null));
+        } catch (TelegramApiException e) {
+            log.error("Error setting bot's commands list " + e.getMessage());
+        }
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public String getBotUsername() {
+        return config.getBotName();
+    }
+
+    @Override
+    public String getBotToken() {
+        return config.getBotToken();
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+
+        if (update.hasMessage() && update.getMessage().hasText()) {
+
+            String messageText = update.getMessage().getText();
+            long chatId = update.getMessage().getChatId();
+
+            if (messageText.contains("/send") && config.getOwnerId() == chatId) {
+                var textToSend = EmojiParser.parseToUnicode(messageText.substring(messageText.indexOf(" ")));
+                var users = userRepository.findAll();
+
+                for (User user : users) {
+                    sendMessage(user.getChatId(), textToSend);
+                }
+            }
+
+            switch (messageText) {
+                case "/start":
+                    registerUser(update.getMessage());
+                    startCommandReceived(chatId, update.getMessage().getChat().getFirstName());
+                    break;
+                case "/help":
+                    sendMessage(chatId, HELP_TEXT + image);
+                    break;
+                case  "/send":
+                    break;
+                case "/register":
+                    register(chatId);
+                    break;
+                default:
+                    sendMessage(chatId, "Все подарки в разработке, пожалуйста обратитесь попозже Хо-Хо-Хо ");
+
+            }
+        } else if (update.hasCallbackQuery()) {
+
+            String callBackData = update.getCallbackQuery().getData();
+            long messageId = update.getCallbackQuery().getMessage().getMessageId();
+            long chaId = update.getCallbackQuery().getMessage().getChatId();
+
+            if (callBackData.equals("YES_BUTTON")) {
+                String text = "You pressed YES button";
+                EditMessageText message = new EditMessageText();
+                message.setChatId(String.valueOf(chaId));
+                message.setText(text);
+                message.setMessageId((int) messageId);
+
+                try {
+                    execute(message);
+                } catch (TelegramApiException e) {
+                    log.error("Error occurred: " + e.getMessage());
+                }
+
+            } else if (callBackData.equals("NO_BUTTON")) {
+                String text = "You pressed NO button";
+                EditMessageText message = new EditMessageText();
+                message.setChatId(String.valueOf(chaId));
+                message.setText(text);
+                message.setMessageId((int) messageId);
+
+                try {
+                    execute(message);
+                } catch (TelegramApiException e) {
+                    log.error("Error occurred: " + e.getMessage());
+                }
+            }
+        }
+    }
+
+
+    private void register(long chatId) {
+
+        SendMessage message = new SendMessage();
+        message.setChatId(String.valueOf(chatId));
+        message.setText("Do you really want to register?");
+        //Кнопки в сообщении
+        InlineKeyboardMarkup markupInLine = new InlineKeyboardMarkup();
+        List<List<InlineKeyboardButton>> rowsInline = new ArrayList<>();
+        List<InlineKeyboardButton> rowInLine = new ArrayList<>();
+
+        var yesButton = new InlineKeyboardButton();
+        yesButton.setText("Yes");
+        yesButton.setCallbackData("YES_BUTTON");
+
+        var noButton = new InlineKeyboardButton();
+        noButton.setText("No");
+        noButton.setCallbackData("NO_BUTTON");
+
+        rowInLine.add(yesButton);
+        rowInLine.add(noButton);
+
+        rowsInline.add(rowInLine);
+
+        markupInLine.setKeyboard(rowsInline);
+        message.setReplyMarkup(markupInLine);
+
+        try {
+            execute(message);
+        } catch (TelegramApiException e) {
+            log.error("Error occurred: " + e.getMessage());
+        }
+
+    }
+
+    private void registerUser(Message msg) {
+
+        if (userRepository.findById(msg.getChatId()).isEmpty()) {
+
+            var chatId = msg.getChatId();
+            var chat = msg.getChat();
+
+            User user = new User();
+            user.setChatId(chatId);
+            user.setFirstName(chat.getFirstName());
+            user.setLastName(chat.getLastName());
+            user.setUserName(chat.getUserName());
+            user.setRegisteredAt(new Timestamp(System.currentTimeMillis()));
+
+            userRepository.save(user);
+            log.info("user saved:" + user);
+        }
+    }
+
+    private void startCommandReceived(long chatId, String name) {
+
+
+        String answer = EmojiParser.parseToUnicode(" Добро пожаловать в игру " + name +
+                " \"Тайный Санта\"!\n" + ":santa:" + "\n" +
+                "Дорогие друзья, приглашаем вас принять участие в веселом и таинственном обмене подарками! " +
+                "Каждый из вас получит имя человека, которому нужно будет тайно подготовить подарок. " +
+                "Помните, ключ к успеху – это сохранение анонимности до самого конца!");
+
+
+        log.info("Replied to user " + name);
+
+        sendPhoto(chatId, image);
+        sendMessage(chatId, answer);
+
+    }
+
+    private void sendMessage(long chatId, String textToSend) {
+        SendMessage message = new SendMessage();
+        message.setChatId(String.valueOf(chatId));
+        message.setText(textToSend);
+
+        //Кнопки в клавиатуре
+        ReplyKeyboardMarkup keyboardMarkup = new ReplyKeyboardMarkup();
+
+        List<KeyboardRow> keyboardRows = new ArrayList<>();
+
+        KeyboardRow row = new KeyboardRow();
+
+        row.add("Все подарки в разработке, пожалуйста обратитесь попозже Хо-Хо-Хо");
+        row.add("Все подарки в разработке, пожалуйста обратитесь попозже Хо-Хо-Хо");
+
+        keyboardRows.add(row);
+
+        row = new KeyboardRow();
+        row.add("Все подарки в разработке, пожалуйста обратитесь попозже Хо-Хо-Хо");
+        row.add("Все подарки в разработке, пожалуйста обратитесь попозже Хо-Хо-Хо");
+        row.add("Все подарки в разработке, пожалуйста обратитесь попозже Хо-Хо-Хо");
+
+        keyboardRows.add(row);
+
+        keyboardMarkup.setKeyboard(keyboardRows);
+
+        message.setReplyMarkup(keyboardMarkup);
+
+        try {
+            execute(message);
+        } catch (TelegramApiException e) {
+            log.error("Error occurred: " + e.getMessage());
+        }
+    }
+
+    private void sendPhoto(long chatId, String imagePath) {
+        SendPhoto photo = new SendPhoto();
+        photo.setChatId(String.valueOf(chatId));
+        photo.setPhoto(new InputFile(imagePath));
+
+        try {
+            execute(photo);
+        } catch (TelegramApiException e) {
+            log.error("Error occurred: " + e.getMessage());
+        }
+    }
+
+
+}
